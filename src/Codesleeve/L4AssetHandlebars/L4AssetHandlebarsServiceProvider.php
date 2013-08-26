65b37b86af7ae4016cd67d2a0a81dd32c00bff2c
Kelt Dockins <kelt@dockins.org>
2013-08-26 11:23:58 -0500

    first commit

---
+++ src/Codesleeve/L4AssetHandlebars/L4AssetHandlebarsServiceProvider.php
@@ -0,0 +1,62 @@
+<?php namespace Codesleeve\L4AssetHandlebars;
+
+use Illuminate\Support\ServiceProvider;
+
+class L4AssetHandlebarsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$count = 1;
+		$project_base = $this->app['path.base'];
+
+		$base = realpath(__DIR__ . '/../../../assets');
+		$base = str_replace($project_base, '', $base, $count);
+
+		$this->package('codesleeve/l4-asset-handlebars');
+
+		\Event::listen('assets.register.paths', function($paths) use ($base) {
+			$paths->add($base . '/javascripts', 'javascripts');
+			$paths->add($base . '/stylesheets', 'stylesheets');
+		});
+
+		\Event::listen('assets.register.filters', function($filters) {
+			$filters->add('.jst.hbs', array(
+				new \Codesleeve\L4AssetHandlebars\Filters\HandlebarsFilter
+			));
+		});
+
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
